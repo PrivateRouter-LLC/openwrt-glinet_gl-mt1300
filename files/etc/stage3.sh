aef27d61bbdc0cbd36837e83ff6d07ed82ae5c7d
Jason Hawks <jason@torguard.net>
2023-05-15 19:55:17 -0400

    Correct some incorrects

--- files/etc/stage3.sh
+++ files/etc/stage3.sh
@@ -1,18 +1,37 @@
 #!/usr/bin/env bash
 # Stage 3 booster to prepare router for first real boot
 
-# Verify we are connected to the Internet
-is_connected() {
-    ping -q -c3 1.1.1.1 >/dev/null 2>&1
-    return $?
-}
-
 # Log to the system log and echo if needed
 log_say()
 {
-    echo "${1}"
-    logger "${1}"
+    SCRIPT_NAME=$(basename "$0")
+    echo "${SCRIPT_NAME}: ${1}"
+    logger "${SCRIPT_NAME}: ${1}"
 }
+
+# Command to wait for Internet connection
+wait_for_internet() {
+    while ! ping -q -c3 1.1.1.1 >/dev/null 2>&1; do
+        log_say "Waiting for Internet connection..."
+        sleep 1
+    done
+    log_say "Internet connection established"
+}
+
+# Wait for Internet connection
+wait_for_internet
+
+# Command to wait for opkg to finish
+wait_for_opkg() {
+  while pgrep -x opkg >/dev/null; do
+    log_say "Waiting for opkg to finish..."
+    sleep 1
+  done
+  log_say "opkg is released, our turn!"
+}
+
+# Wait for opkg to finish
+wait_for_opkg
 
 log_say "                                                                      "
 log_say " ███████████             ███                         █████            "
@@ -37,13 +56,6 @@ log_say "░░░░░   ░░░░░  ░░░░░░    ░░░░�
 # Set our router's dns
 echo "nameserver 1.1.1.1" > /etc/resolv.conf
 
-# Check and wait for Internet connection
-while ! is_connected; do
-    log_say "Waiting for Internet connection..."
-    sleep 1
-done
-log_say "Internet connection established"
-
 # Cleanup our auto-provision and prepare for first real boot
 [ -d /etc/auto-provision ] && rm -rf /etc/auto-provision
 [ -f /etc/rc.local ] && echo "# Empty by design" > /etc/rc.local
@@ -53,12 +65,9 @@ log_say "Downloading startup.tar.gz"
 wget -q -O /tmp/startup.tar.gz https://github.com/PrivateRouter-LLC/script-repo/raw/main/startup-scripts/startup.tar.gz
 log_say "Extracting startup.tar.gz"
 tar -xzf /tmp/startup.tar.gz -C /etc
-log_say "Enabling our rc.custom startup script"
-/etc/init.d/custom disable
-/etc/init.d/custom enable
-log_say "Running our rc.custom startup script"
-bash /etc/rc.custom
-log_say "We are done with our first boot and should be setup to run correctly!"
 
-# We are done, remove ourself by script name
-rm -- "$0"
+log_say "Removing our script before reboot"
+rm "$0"
+
+log_say "Reboot to uptake our rc.custom boot script"
+reboot
